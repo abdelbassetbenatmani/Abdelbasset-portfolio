ee9df2dca8cf4d73a1f88b0c40127e1a667b56b6
abdelbassetbenatmani <abdelbassetbenatmani@gmail.com>
2023-12-23 13:41:00 +0100

    optimize my portofolio

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline";
+import { CodeBracketIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 
 const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
@@ -20,7 +20,7 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
         </div>
       </div>
       <div className="text-white rounded-b-xl mt-3 bg-[#181818]py-6 px-4">
-        <h5 className="text-xl font-semibold mb-2">{title}</h5>
+        <p className="text-xl font-semibold mb-2">{title}</p>
         <p className="text-[#ADB7BE]">{description}</p>
       </div>
     </div>
